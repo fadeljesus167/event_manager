5437d18194f966a3c5fb89cbe15d50ea3e2a4177
Fadel <fadeljesus@gmail.com>
2025-03-24 16:52:44 -0400

    step 4

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,8 +1,43 @@
+require 'csv'
+require 'google-apis-civicinfo_v2'
+
+def clean_zipcode(zipcode)
+  zipcode.to_s.rjust(5, '0')[0..4]
+end
+
+def legislators_by_zipcode(zipcode)
+  civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+  civic_info.key = File.read('secret.key').strip
+
+  begin
+    legislators = civic_info.representative_info_by_address(
+      address: zipcode,
+      levels: 'country',
+      roles: ['legislatorUpperBody', 'legislatorLowerBody']
+    )
+
+    legislators = legislators.officials
+
+    legislators_names = legislators.map(&:name).join(", ")
+  rescue
+    'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+  end
+end
+
 puts "Event Manager Initialized!"
 
-lines = File.readlines('event_attendees.csv')
-lines.each do |line|
-  columns = line.split(",")
-  name = columns[2]
-  puts name
+template_letter = File.read('form_letter.html')
+
+contents = CSV.open(
+  'event_attendees.csv',
+  headers: true,
+  header_converters: :symbol  
+)
+
+contents.each do |row|
+  name = row[:first_name]
+  zipcode = clean_zipcode(row[:zipcode])
+  legislators = legislators_by_zipcode(zipcode)
+  
+  puts "#{name} #{zipcode} #{legislators}"
 end
